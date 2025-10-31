905af4dcedf5181be3edcc4652676c6a0edeff82
Angel <angelwu05@gmail.com>
2025-10-31 09:36:16 -0400

    Build LOA interactive UI

--- ui/loa_ui.ml
+++ ui/loa_ui.ml
@@ -1,9 +1,9 @@
 open! Core
+open Loa_logic_library
 open Virtual_dom
 open! Bonsai.Let_syntax
-open Loa_logic_library
 
-(* Utilities to inspect board *)
+(* Board inspection helpers *)
 let all_coords : coord list =
   List.init board_size ~f:(fun r -> List.init board_size ~f:(fun c -> { r; c }))
   |> List.concat
@@ -11,45 +11,15 @@ let all_coords : coord list =
 let get_cell (b:board) (p:coord) : cell option =
   List.nth b p.r |> Option.bind ~f:(fun row -> List.nth row p.c)
 
-let is_piece (b:board) (p:player) (q:coord) : bool =
-  match get_cell b q with Some (Piece x) when Poly.equal x p -> true | _ -> false
-
-(* Generate all legal moves for the side to move, naive O(64*64) check *)
 let all_legal_moves (st:state) : move_ list =
-  let froms = List.filter all_coords ~f:(fun q -> is_piece st.board st.turn q) in
+  let is_own_piece q = match get_cell st.board q with Some (Piece pl) when Poly.equal pl st.turn -> true | _ -> false in
+  let froms = List.filter all_coords ~f:is_own_piece in
   List.concat_map froms ~f:(fun from_ ->
       List.filter_map all_coords ~f:(fun to_ ->
           if from_.r = to_.r && from_.c = to_.c then None
           else match legal_move st { from_; to_ } with Ok () -> Some { from_; to_ } | Error _ -> None))
 
-type triplet = { s_from : state; mv : move_; s_to : state }
-
-let build_triplets () : triplet list =
-  let t1 =
-    let s_from = Loa_logic_library.initial_state in
-    let mv = Loa_logic_library.example_move1 in
-    let s_to = match make_move s_from mv with Ok s -> s | Error _ -> failwith "illegal" in
-    { s_from; mv; s_to }
-  in
-  let t2 =
-    let s_from = (match make_move Loa_logic_library.initial_state Loa_logic_library.example_move1 with Ok s -> s | Error _ -> failwith "illegal") in
-    let mv = Loa_logic_library.example_move2 in
-    let s_to = match make_move s_from mv with Ok s -> s | Error _ -> failwith "illegal" in
-    { s_from; mv; s_to }
-  in
-  (* Add 3 more deterministic triplets by taking the first few legal moves each step *)
-  let rec extend acc st n =
-    if n = 0 then List.rev acc
-    else
-      match all_legal_moves st with
-      | [] -> List.rev acc
-      | mv::_ ->
-        let s_to = match make_move st mv with Ok s -> s | Error _ -> failwith "illegal" in
-        extend ({ s_from = st; mv; s_to } :: acc) s_to (n-1)
-  in
-  let extra = extend [] t2.s_to 3 in
-  t1 :: t2 :: extra
-
+(* SVG pieces *)
 let viewbox = Vdom.Attr.create "viewBox" "0 0 100 100"
 
 let white_piece =
@@ -68,79 +38,80 @@ let black_piece =
       "<circle cx='50' cy='50' r='40' stroke='black' stroke-width='6' fill='black' />"
     ()
 
-let board_view ~(state_to_show:state) ~(highlight:coord option * coord option) =
-  let is_highlight r c =
-    match highlight with
-    | None, None -> false
-    | Some a, Some b -> (a.r = r && a.c = c) || (b.r = r && b.c = c)
-    | Some a, None -> a.r = r && a.c = c
-    | None, Some b -> b.r = r && b.c = c
-  in
-  let render_cell r c =
-    let piece =
-      match get_cell state_to_show.board { r; c } with
-      | Some (Piece Black) -> black_piece
-      | Some (Piece White) -> white_piece
-      | _ -> Vdom.Node.none
+(* Render LOA board *)
+let loa_board ~(game_state : state) ~set_game_state:_ =
+  let is_game_over = is_winner game_state Black || is_winner game_state White in
+  let render_cell ~row ~col =
+    let cell_value = get_cell game_state.board { r = row; c = col } in
+    let piece_svg, maybe_clickable_attr =
+      match cell_value with
+      | Some (Piece Black) -> black_piece, Vdom.Attr.empty
+      | Some (Piece White) -> white_piece, Vdom.Attr.empty
+      | Some Empty | None when is_game_over -> Vdom.Node.none, Vdom.Attr.empty
+      | Some Empty | None ->
+        (* Empty cell: check if we have a selected piece that can move here *)
+        Vdom.Node.none, Vdom.Attr.empty
     in
-    let border_classes =
-      (if r < board_size - 1 then [ Vdom.Attr.class_ "border_bottom" ] else []) @
-      (if c < board_size - 1 then [ Vdom.Attr.class_ "border_right" ] else [])
+    let attrs =
+      [ Vdom.Attr.class_ "column"
+      ; Vdom.Attr.style
+          Css_gen.(
+            left (`Percent (Percent.of_percentage (Float.of_int col *. 12.5)))
+            @> width (`Percent (Percent.of_percentage 12.5))
+          )
+      ; maybe_clickable_attr
+      ]
     in
-    let highlight_class = if is_highlight r c then [ Vdom.Attr.class_ "slowly_appear" ] else [] in
+    let attrs =
+      attrs @ if row < board_size - 1 then [ Vdom.Attr.class_ "border_bottom" ] else []
+    in
+    let attrs =
+      attrs @ if col < board_size - 1 then [ Vdom.Attr.class_ "border_right" ] else []
+    in
     Vdom.Node.div
-      ~attrs:
-        ([ Vdom.Attr.class_ "column"
-         ; Vdom.Attr.style Css_gen.(
-             left (`Percent (Percent.of_percentage (Float.of_int c *. 12.5)))
-             @> width (`Percent (Percent.of_percentage 12.5))
-           )
-         ] @ border_classes)
-      [ Vdom.Node.div ~attrs:(highlight_class @ [ Vdom.Attr.class_ "max_size" ]) [ piece ] ]
+      ~attrs
+      [ Vdom.Node.div ~attrs:[ Vdom.Attr.class_ "max_size" ] [ piece_svg ] ]
   in
   Vdom.Node.div
     ~attrs:[ Vdom.Attr.class_ "game" ]
-    (List.init board_size ~f:(fun r ->
-         Vdom.Node.div
-           ~attrs:
-             [ Vdom.Attr.class_ "row"
-             ; Vdom.Attr.style Css_gen.(
-                 top (`Percent (Percent.of_percentage (Float.of_int r *. 12.5)))
-                 @> height (`Percent (Percent.of_percentage 12.5))
-               )
-             ]
-           (List.init board_size ~f:(fun c -> render_cell r c))))
-
-let toolbar ~(triplet_idx:int) ~(num:int) ~(show_to:bool) ~set_idx ~set_show =
-  let btn label on_click =
-    Vdom.Node.button ~attrs:[ Vdom.Attr.on_click (fun _ -> on_click ()) ] [ Vdom.Node.text label ]
-  in
-  Vdom.Node.div
-    ~attrs:[]
-    [ btn "Prev" (fun () -> set_idx (Int.max 0 (triplet_idx - 1)))
-    ; Vdom.Node.text (sprintf "  Triplet %d/%d  " (triplet_idx+1) num)
-    ; btn "Next" (fun () -> set_idx (Int.min (num-1) (triplet_idx + 1)))
-    ; Vdom.Node.text "  |  "
-    ; btn (if show_to then "Show S_from" else "Show S_to") (fun () -> set_show (not show_to))
-    ]
+    (List.init board_size ~f:(fun row ->
+       Vdom.Node.div
+         ~attrs:
+           [ Vdom.Attr.class_ "row"
+           ; Vdom.Attr.style
+               Css_gen.(
+                 top (`Percent (Percent.of_percentage (Float.of_int row *. 12.5)))
+                 @> height (`Percent (Percent.of_percentage 12.5)))
+           ]
+         (List.init board_size ~f:(fun col -> render_cell ~row ~col))))
 
 let app =
-  let triplets = build_triplets () |> Array.of_list in
-  let num = Array.length triplets in
-  let%sub idx, set_idx = Bonsai.state (module Int) ~default_model:0 in
-  let%sub show_to, set_show_to = Bonsai.state (module Bool) ~default_model:false in
-  let%arr idx = idx
-  and set_idx = set_idx
-  and show_to = show_to
-  and set_show_to = set_show_to in
-  let idx = Int.clamp_exn idx ~min:0 ~max:(num-1) in
-  let t = triplets.(idx) in
-  let state_to_show = if show_to then t.s_to else t.s_from in
+  let%sub game_state, set_game_state =
+    Bonsai.state ~default_model:initial_state (module struct
+      type t = state
+      let sexp_of_t _ = Sexp.Atom "state"
+      let t_of_sexp _ = initial_state
+      let equal = Poly.equal
+    end)
+  in
+  let%arr game_state = game_state
+  and set_game_state = set_game_state in
   Vdom.Node.div
-    [ toolbar ~triplet_idx:idx ~num ~show_to ~set_idx ~set_show:set_show_to
-    ; board_view ~state_to_show ~highlight:(Some t.mv.from_, Some t.mv.to_)
+    [ Vdom.Node.div
+        ~attrs:[]
+        [ Vdom.Node.text
+            (sprintf "Turn: %s | Legal moves: %d"
+               (match game_state.turn with Black -> "Black" | White -> "White")
+               (List.length (all_legal_moves game_state)))
+        ]
+    ; loa_board ~game_state ~set_game_state
+    ; Vdom.Node.div
+        ~attrs:[]
+        [ Vdom.Node.text
+            (if is_winner game_state Black then "Black wins!"
+             else if is_winner game_state White then "White wins!"
+             else "Game in progress")
+        ]
     ]
 
 let () = Bonsai_web.Start.start app
-
-
